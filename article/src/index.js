650ddb82a7dea1c69a13a15d8d304a100975ca61
5nax <97379931+5nax@users.noreply.github.com>
2025-02-24 10:58:16 +0545

    Update index.js

--- article/src/index.js
+++ article/src/index.js
@@ -4,12 +4,9 @@ import App from './App';
 import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient';
 
-// Get the container element
 const container = document.getElementById('root');
-// Create the root using the container
 const root = ReactDOM.createRoot(container);
 
-// Render your application; note that we don’t pass the container again
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
